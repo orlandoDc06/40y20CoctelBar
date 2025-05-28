09985360bcd98dbad138a6436f56ceea8b81ca44
Orlando Del Cid <orlandc410@gmail.com>
2025-05-28 00:01:05 -0600

    Cierre de Sesion

--- app/src/main/java/com/example/a40y20coctelbar/Menus/CocineroMenu.java
+++ app/src/main/java/com/example/a40y20coctelbar/Menus/CocineroMenu.java
@@ -1,6 +1,10 @@
 package com.example.a40y20coctelbar.Menus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.a40y20coctelbar.MainActivity;
 import com.example.a40y20coctelbar.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CocineroMenu extends AppCompatActivity {
+
+    private FirebaseAuth miAuth;
+    private Button btnCerrarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,29 @@ public class CocineroMenu extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        miAuth = FirebaseAuth.getInstance();
+        btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+
+
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrarSesion();
+            }
+        });
     }
+
+    private void cerrarSesion() {
+        miAuth.signOut();
+
+        Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(CocineroMenu.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/a40y20coctelbar/Menus/MeseroMenu.java
+++ app/src/main/java/com/example/a40y20coctelbar/Menus/MeseroMenu.java
@@ -3,6 +3,8 @@ package com.example.a40y20coctelbar.Menus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.a40y20coctelbar.MainActivity;
 import com.example.a40y20coctelbar.OpcionesAdministrador.GestionarUsuarios;
 import com.example.a40y20coctelbar.OpcionesAdministrador.MenuProductos;
 import com.example.a40y20coctelbar.OpcionesAdministrador.ReglasNegocio;
@@ -19,10 +22,13 @@ import com.example.a40y20coctelbar.OpcionesMesero.CrearComanda;
 import com.example.a40y20coctelbar.OpcionesMesero.HistorialComanda;
 import com.example.a40y20coctelbar.OpcionesMesero.MenuComanda;
 import com.example.a40y20coctelbar.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MeseroMenu extends AppCompatActivity {
 
     CardView crearComandaCard, historialComanda, verMenuCard;
+    private Button btnCerrarSesion;
+    private FirebaseAuth miAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +38,9 @@ public class MeseroMenu extends AppCompatActivity {
         crearComandaCard = findViewById(R.id.cardCrearComanda);
         historialComanda = findViewById(R.id.cardModificarComanda);
         verMenuCard = findViewById(R.id.cardMenuProductos);
+        btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
 
+        miAuth = FirebaseAuth.getInstance();
 
         crearComandaCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +66,25 @@ public class MeseroMenu extends AppCompatActivity {
             }
         });
 
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrarSesion();
+            }
+        });
 
     }
+
+    private void cerrarSesion() {
+        miAuth.signOut();
+
+        Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(MeseroMenu.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+        finish();
+    }
+
 }
